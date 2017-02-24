b17252c8e151729d060cda198c151871019bb5f3
Paul J. Adam <paul@pauljadam.com>
2017-02-24 11:16:20 -0600

    na

--- FormBadiOS/FormBadiOS/ViewController.swift
+++ FormBadiOS/FormBadiOS/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Add gesture recognizer to hide keyboard when tapping outside text fields.
+        tapToDismissKeyboard()
+    }
+    func tapToDismissKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
     @IBAction func submitForm(_ sender: Any) {
         errorText.isHidden = false
